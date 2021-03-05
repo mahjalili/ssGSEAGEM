97194c1deb6ee418975495540614c631ed7e722a
mahjalili <50863023+mahjalili@users.noreply.github.com>
2021-03-05 20:15:15 +0330

    Update inits.m

--- Matlab/inits.m
+++ Matlab/inits.m
@@ -1,6 +1,10 @@
+% Initialization
+% A conceptual framework for transcriptional data integration into a genome-scale metabolic model.
+%
+% Author: Mahdi Jalili, 2021
+
 clear all; clc;
 
-% Initialization
 COBRASOLVER = 'ibm_cplex'; % glpk | mosek | ibm_cplex | gurobi | pdco
 changeCobraSolver(COBRASOLVER, 'all');
 % Add paths

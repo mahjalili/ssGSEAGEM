65ea0b04bc0192e6c97072afd5710c2184c4921a
mahjalili <50863023+mahjalili@users.noreply.github.com>
2021-03-05 20:24:50 +0330

    Update loadDataset.m

--- Matlab/utils/loadDataset.m
+++ Matlab/utils/loadDataset.m
@@ -1,11 +1,22 @@
-function datasets = loadDataset(datasetname, organism, dataname)
 % Load one of the datasets.
+% A conceptual framework for transcriptional data integration into a genome-scale metabolic model.
 %
+% INPUTS
+%       datasetname - 'GIMME', 'ssGSEA'
+%       organism - 'Yeast'
+%       dataname - 'Grigaitis', 'GSE8895'
+%
+% OUTPUTS
+%       datasets - reaction expression levels
+%
+% Author: Mahdi Jalili, 2021
+
+function datasets = loadDataset(datasetname, organism, dataname)
     clear datasets;
     datasets = cell(0);
     data = load(['datasets/',dataname,'/',datasetname,'/expRxns',organism,'.mat']);
     fields = fieldnames(data.expRxns);
     for i = 1:length(fields)
-        datasets{i} = struct('name', fields{i}, 'expRxnsMinMax', data.expRxns.(fields{i}).MinMax, 'Q25', data.expRxns.(fields{i}).Q25, 'Q75', data.expRxns.(fields{i}).Q75);
+        datasets{i} = struct('name', fields{i}, 'expRxnsMinMax', data.expRxns.(fields{i}).MinMax);
     end
 end
